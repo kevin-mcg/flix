0219b02a1d957bc125263b1f2bb3b1d51cc64dd9
kevin-mcg <kevin.m.mcguinness@gmail.com>
2026-07-25 14:48:21 +0200

    Sessions controller and sign in

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   resources :movies do
     resources :reviews
   end
+  resource :session, only: [:new, :create, :destroy]
+
   resources :users
 
   get "signup" => "users#new"
-
+  get "signin" => "sessions#new"
 end
