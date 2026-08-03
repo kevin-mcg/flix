e11e33fd126f91d6576b7be29a64bbde2699530b
kevin-mcg <kevin.m.mcguinness@gmail.com>
2026-08-03 23:38:39 +0100

    Part 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -26,11 +27,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user), notice: "You account was successfully updated!"
     else
@@ -40,7 +39,6 @@ class UsersController < ApplicationController
 
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     session[:user_id] = nil
     # The alert msg does not show using alert or notice...
@@ -53,5 +51,10 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).
             permit(:name, :username, :email, :password, :password_confirmation)
+  end
+
+  def require_correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url, status: :see_other unless current_user?(@user)
   end
 end
