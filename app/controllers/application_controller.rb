0a50504df921bf4125482f9c360e19d50e25ccf5
kevin-mcg <kevin.m.mcguinness@gmail.com>
2026-08-03 15:21:22 +0100

    Part 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+
+  def require_signin
+    unless current_user
+      redirect_to new_session_url, alert: "Please sign in first!"
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-
-  def current_user
-    # memoization - to avoid additional cache calls to fid the user...
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
 end
