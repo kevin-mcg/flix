fcc96534038c4f2277d9c5600f9f494eefde0681
kevin-mcg <kevin.m.mcguinness@gmail.com>
2026-08-06 14:20:42 +0100

    Migration removing names and adding the user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_08_04_140637) do
+ActiveRecord::Schema[7.2].define(version: 2026_08_06_131444) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
@@ -28,12 +28,12 @@ ActiveRecord::Schema[7.2].define(version: 2026_08_04_140637) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string "name"
     t.integer "stars"
     t.text "comment"
     t.integer "movie_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
   end
 
